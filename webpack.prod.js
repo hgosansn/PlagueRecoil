ca02a104303464b97d69d4a50f5789bd9718b1d6
Hugo SANSON <hugo@agorapulse.com>
2024-08-18 09:00:37 +0200

    This commit has no message. It’s a mystery.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config.js');
 const TerserPlugin = require('terser-webpack-plugin');
+const env = require('./environment');
 const path = require('path');
 
 prod = true;
@@ -20,9 +21,9 @@ module.exports = merge(common, {
         ],
     },
     output: {
+        ...common.output,
         filename: '[name].[chunkhash].js', // Enable cache-busting
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-        publicPath: env.publicPath,
     },
 });
